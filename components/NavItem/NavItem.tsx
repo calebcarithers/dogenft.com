d7fe8d87b085f52aec50830f06d2a6cda662cd4d
Caleb Carithers <calebcarithers@me.com>
2022-04-20 16:14:38 -0400

    basic scroll

---
+++ components/NavItem/NavItem.tsx
@@ -0,0 +1,18 @@
+import {PropsWithChildren} from "react";
+import {css} from "../../helpers/css";
+
+interface NavItemProps {
+  isSelected?: boolean;
+  onClick?: () => void;
+}
+
+const NavItem: React.FC<PropsWithChildren<NavItemProps>> = ({
+                                                              isSelected = false,
+                                                              onClick,
+                                                              children}) => {
+  return <div className={css("hover:cursor-pointer", "hover:underline")} onClick={onClick && onClick}>
+    {children}
+  </div>
+}
+
+export default NavItem
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,8 +2,33 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import {css} from "../helpers/css";
 import Button from "../components/Button/Button";
+import NavItem from "../components/NavItem/NavItem";
+import {useCallback, useEffect, useRef, useState} from "react";
+import Image from "next/image"
 
 const Home: NextPage = () => {
+  const navItems = ['Doge', 'The Doge NFT', '$DOG', 'DAOge', 'Bark Tank']
+
+
+  const _navItems = [
+    {title: 'Doge', id: "doge"},
+    {title: 'The Doge NFT', id: "dogenft"},
+    {title: '$DOG', id: "dog"},
+    {title: 'DAOge', id: "daoge"},
+    {title: 'Bark Tank', id: "barktank"}
+  ]
+
+  const [fullSize, setFullSize] = useState(0)
+
+  const containerRef = useCallback<any>((node: HTMLDivElement) => {
+    if (node) {
+      setFullSize(node.clientHeight)
+      window.addEventListener('resize', () => {
+        setFullSize(node.clientHeight)
+      })
+    }
+  }, [])
+
   return (
     <div className={css("bg-white", "text-black", "grow", "flex", "flex-col", "p-3")}>
       <Head>
@@ -15,12 +40,12 @@ const Home: NextPage = () => {
       <main className={css("grow", "font-bold", "grid", "grid-cols-12")}>
         <div className={css("flex", "flex-col", "justify-between", "col-span-3")}>
           <div className={css("flex", "items-center", "justify-center", "grow")}>
-            <div className={css("text-4xl")}>
-              <div className={css("mb-12")}>Doge</div>
-              <div className={css("mb-12")}>The Doge NFT</div>
-              <div className={css("mb-12")}>$DOG</div>
-              <div className={css("mb-12")}>DAOge</div>
-              <div>Bark Tank</div>
+            <div className={css("text-4xl", "flex", "flex-col", "gap-7")}>
+              {_navItems.map(item => <NavItem key={item.id} onClick={() => {
+                document.getElementById(item.id)?.scrollIntoView({behavior: "smooth"})
+              }}>
+                {item.title}
+              </NavItem>)}
             </div>
           </div>
           <div className={css("flex", "flex-col", "items-start", "gap-4", "py-5")}>
@@ -32,8 +57,30 @@ const Home: NextPage = () => {
         <div className={css("flex", "justify-center")}>
           <div className={css("border-grey", "border-dashed", "col-span-1")} style={{width: "1px", borderWidth: "1px"}}/>
         </div>
-        <div className={css("flex", "justify-center", "items-center", "col-span-8")}>
-          this is doge content
+        <div className={css("col-span-8", "text-4xl", "overflow-x-hidden", "text-center")} ref={containerRef}>
+          <div style={{maxHeight: "300px"}}>
+            <div id={"doge"} className={css("flex", "items-center", "justify-center", "bg-red-100")} style={{height: `${fullSize}px`}}>
+              <div>
+                <div className={css("relative", "w-full", "h-full", "hover:cursor-pointer", "active:translate-x-1", "active:translate-y-1")} style={{height: "500px"}}>
+                  <Image src={"/kabosu.png"} layout={"fill"} objectFit={"contain"}/>
+                </div>
+                <div className={css("text-2xl", "mt-10")}>
+                  The Mona Lisa of the internet, Doge, grew to infamy in the early 2000's when Atsuko Sato posted 8 photos to her blog of her adopted Shiba Inu, Kabosu.</div>
+              </div>
+            </div>
+            <div id={"dogenft"} className={css("flex", "items-center", "justify-center", "bg-red-200")} style={{height: `${fullSize}px`}}>
+              doge nft
+            </div>
+            <div id={"dog"} className={css("flex", "items-center", "justify-center", "bg-red-300")} style={{height: `${fullSize}px`}}>
+              dog stuff
+            </div>
+            <div id={"daoge"} className={css("flex", "items-center", "justify-center", "bg-red-400")} style={{height: `${fullSize}px`}}>
+              daoge
+            </div>
+            <div id={"barktank"} className={css("flex", "items-center", "justify-center", "bg-red-500")} style={{height: `${fullSize}px`}}>
+              barktank
+            </div>
+          </div>
         </div>
       </main>
 
