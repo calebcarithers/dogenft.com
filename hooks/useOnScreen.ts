4211d22cd5df2446e3b0ce0ad07fe5e8d34f23fe
Caleb Carithers <calebcarithers@me.com>
2022-04-26 22:45:48 -0400

    init observer in hook so next doesn't throw on build

--- hooks/useOnScreen.ts
+++ hooks/useOnScreen.ts
@@ -2,15 +2,15 @@ import {useEffect, useState} from "react";
 
 export default function useOnScreen(ref: React.RefObject<any>) {
   const [isIntersecting, setIntersecting] = useState(false)
-  const observer = new IntersectionObserver(
-    ([entry]) => setIntersecting(entry.isIntersecting),
-    {
-      root: document.querySelector("#root"),
-      threshold: 0.5
-    }
-  )
 
   useEffect(() => {
+    const observer = new IntersectionObserver(
+      ([entry]) => setIntersecting(entry.isIntersecting),
+      {
+        root: document.querySelector("#root"),
+        threshold: 0.5
+      }
+    )
     observer.observe(ref.current)
     return () => { observer.disconnect() }
   }, [ref.current])
