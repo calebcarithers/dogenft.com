7299f4bec9756cf6e0210603016469f831f34e5e
Caleb Carithers <calebcarithers@me.com>
2022-05-23 16:37:03 -0400

    up

--- layouts/Home/Home.layout.tsx
+++ layouts/Home/Home.layout.tsx
@@ -26,14 +26,14 @@ const Divider = () => {
 
 export const Background = () => {
     return <div className={css("hidden", "md:flex", "absolute", "w-full", "h-full", "left-0", "top-0", "justify-between", "flex-col")} style={{zIndex: -1}}>
-        <MovingText className={css("text-meme-red")}>what r you doing</MovingText>
+        <MovingText className={css("text-meme-red")}>very dog</MovingText>
         <MovingText className={css("text-meme-green")}>wow</MovingText>
         <MovingText className={css("text-meme-yellow")}>so scare</MovingText>
-        <MovingText className={css("text-meme-green")}>woof</MovingText>
+        <MovingText className={css("text-meme-green")}>much rare</MovingText>
         <MovingText className={css("text-meme-red")}>what r you doing</MovingText>
-        <MovingText className={css("text-meme-magenta")}>wow</MovingText>
-        <MovingText className={css("text-meme-green")}>woof</MovingText>
-        <MovingText className={css("text-meme-green")}>wow</MovingText>
+        <MovingText className={css("text-meme-magenta")}></MovingText>
+        <MovingText className={css("text-meme-green")}>many pixel</MovingText>
+        <MovingText className={css("text-meme-green")}>very fractional</MovingText>
     </div>
 }
 
