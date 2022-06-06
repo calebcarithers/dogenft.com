fb3e5201b75c39ab932650d69f5cf941711c861e
Caleb Carithers <calebcarithers@me.com>
2022-06-06 02:05:53 -0400

    up
    '

--- packages/hardhat/test/sample-test.js
+++ packages/hardhat/test/sample-test.js
@@ -4,24 +4,18 @@ const { ethers } = require("hardhat");
 describe("Greeter", function () {
   it("Should return the new greeting once it's changed", async function () {
     const doge = await ethers.getContractFactory("InDogeWeTrust");
-    const doger = await doge.deploy();
-    await doger.deployed();
+    const IDWT = await doge.deploy();
+    await IDWT.deployed();
 
 
     const signers = await ethers.getSigners()
     const [owner, addr1] = signers
 
-    console.log("debug:: addr1", addr1)
+    const tx = await IDWT.connect(owner).safeMint(addr1.address, "teeesst")
+    console.log("reciept::", await tx.wait())
 
-    // await doge.connect(addr1).safeMint()
+    const balanceTx = await IDWT.connect(owner).balanceOf(addr1.address)
+    console.log("receipt::", balanceTx)
 
-    // expect(await greeter.greet()).to.equal("Hello, world!");
-
-    // const setGreetingTx = await doger.safeMint();
-
-    // wait until the transaction is mined
-    // await setGreetingTx.wait();
-
-    // expect(await greeter.greet()).to.equal("Hola, mundo!");
   });
 });
