059a1037553fba404bfd66c54143d61652523b52
Caleb Carithers <calebcarithers@me.com>
2022-07-04 10:46:19 -0400

    no console

--- packages/hardhat/contracts/InDogeWeTrust.sol
+++ packages/hardhat/contracts/InDogeWeTrust.sol
@@ -1,8 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
 
-import "hardhat/console.sol";
-
 import "@openzeppelin/contracts-upgradeable/token/ERC721/ERC721Upgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/security/PausableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
