52bb9fbe2d550252154bfd2ebb32d41ae3b8ee5a
talentdev2020 <48382274+talentdev2020@users.noreply.github.com>
2022-08-29 11:11:23 -0700

    implement unit test for IDWT (#6)
    * implement unit test for IDWT
    
    * up
    
    Co-authored-by: Caleb Carithers <calebcarithers@me.com>

---
+++ packages/hardhat/test/in-doge-we-trust.js
@@ -0,0 +1,63 @@
+const {expect} = require("chai");
+const {ethers, upgrades} = require("hardhat");
+const {BigNumber} = require("ethers");
+
+describe("In Doge We Trust", function () {
+  let mockPixelContract, signers, whitelisted, tree;
+  let InDogeWeTrust;
+  let tokenId;
+
+  const getTokenIdFromReceipt = (receipt) => {
+    return BigNumber.from(receipt.logs[0].topics[3]).toNumber()
+  }
+
+  before(async () => {
+    signers = await ethers.getSigners()
+    whitelisted = signers.slice(0, 5)
+
+    // This is only for mock pixel contract.
+    console.log("\ndeploying mockPixelContract")
+    const MockPixelContract = await ethers.getContractFactory("MockPixel");
+    mockPixelContract = await upgrades.deployProxy(MockPixelContract, []);
+    await mockPixelContract.deployed();
+    console.log("mockPixelContract deployed to:", mockPixelContract.address);
+
+    console.log("\ndeploying InDogeWeTrust")
+    const InDogeWeTrustFactory = await ethers.getContractFactory("InDogeWeTrust");
+    InDogeWeTrust = await upgrades.deployProxy(InDogeWeTrustFactory, [mockPixelContract.address]);
+    await InDogeWeTrust.deployed();
+  })
+
+  it("mint token and allow only one token", async function () {
+    const signer = whitelisted[1]
+    const tx = await mockPixelContract.connect(signer).mint()
+
+    const receipt = await tx.wait()
+    tokenId = getTokenIdFromReceipt(receipt)
+
+    await InDogeWeTrust.connect(signer).safeMint(tokenId);
+    expect(await InDogeWeTrust.balanceOf(signer.address)).to.equal(1);
+
+    await expect(InDogeWeTrust.connect(signer).safeMint(tokenId + 1)).to.be.revertedWith("Address has already claimed");
+  });
+
+  it("Should check claimed for wallet and pixel id", async function () {
+    expect(await InDogeWeTrust.hasPixelClaimed(tokenId)).to.equal(true);
+    expect(await InDogeWeTrust.hasPixelClaimed(tokenId + 1)).to.equal(false);
+    expect(await InDogeWeTrust.hasClaimed(whitelisted[1].address)).to.equal(true);
+  });
+
+  it("mint token from pixel id only one time", async function () {
+    mockPixelContract["safeTransferFrom(address,address,uint256)"](whitelisted[1].address, whitelisted[2].address, tokenId)
+    await expect(InDogeWeTrust.connect(whitelisted[2]).safeMint(tokenId)).to.be.revertedWith("Pixel already used to claimed");
+  });
+
+   it("should allow pixel owner to mint", async function () {
+    const tx = await mockPixelContract.mint()
+
+    const receipt = await tx.wait()
+    tokenId = getTokenIdFromReceipt(receipt)
+
+    await expect(InDogeWeTrust.connect(whitelisted[2]).safeMint(tokenId)).to.be.revertedWith("You do not own this pixel");
+  });
+});
