8797c13cd73d1a2ed6ffd8d09a562626eda83f30
Caleb Carithers <calebcarithers@me.com>
2022-08-29 11:56:13 -0400

    copy fix

--- packages/frontend/stores/NftRadio.store.ts
+++ packages/frontend/stores/NftRadio.store.ts
@@ -31,7 +31,7 @@ class NftRadioStore {
                 year: "2021",
                 artists: [
                     {name: "Bassjackers", link: "https://twitter.com/Bassjackers"},
-                    {name: "PleasrDao", link: "https://twitter.com/pleasrdao"}
+                    {name: "PleasrDAO", link: "https://twitter.com/pleasrdao"}
                 ],
                 videoSrc: "/videos/in-doge-we-trust.mp4",
                 contractAddress: vars.NEXT_PUBLIC_IDWT_CONTRACT_ADDRESS,
