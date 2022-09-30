cd8e5ee9915fd97ecc17b8f257e8ac4eefa90498
Caleb Carithers <calebcarithers@me.com>
2022-09-30 14:22:52 -0500

    up

--- packages/hardhat/contracts/FractionManager.sol
+++ packages/hardhat/contracts/FractionManager.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
 
-import "@openzeppelin/contracts-upgradeable/security/PausableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 import "@openzeppelin/contracts/utils/introspection/ERC165Checker.sol";
 import "@openzeppelin/contracts-upgradeable/token/ERC1155/IERC1155Upgradeable.sol";
@@ -10,10 +9,11 @@ import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import "@openzeppelin/contracts/token/ERC1155/IERC1155.sol";
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 
-contract FractionManager is Initializable, PausableUpgradeable, OwnableUpgradeable, IERC1155ReceiverUpgradeable {
+contract FractionManager is Initializable, OwnableUpgradeable, IERC1155ReceiverUpgradeable {
     using ERC165Checker for address;
     address public pixelAddress;
     mapping(address => mapping(uint256 => bool)) public pixelClaimed;
+    mapping(address => bool) public isClaimOpen;
 
     /// @custom:oz-upgrades-unsafe-allow constructor
     constructor() {
@@ -21,7 +21,6 @@ contract FractionManager is Initializable, PausableUpgradeable, OwnableUpgradeab
     }
 
     function initialize(address _pixelAddress) initializer public {
-        __Pausable_init();
         __Ownable_init();
         pixelAddress = _pixelAddress;
     }
@@ -32,11 +31,11 @@ contract FractionManager is Initializable, PausableUpgradeable, OwnableUpgradeab
         IERC1155(_erc1155Address).safeTransferFrom(msg.sender, address(this), _fractionId, _amount, "");
     }
 
-    function claim(address _erc1155Address, uint256 _fractionId, uint256 _pixelId) public whenNotPaused {
+    function claim(address _erc1155Address, uint256 _fractionId, uint256 _pixelId) public {
+        require(isClaimOpen[_erc1155Address], "Claim is not open");
         require(!pixelClaimed[_erc1155Address][_pixelId], "Pixel already claimed");
         require(IERC721(pixelAddress).ownerOf(_pixelId) == msg.sender, "Not pixel owner");
         require(IERC1155(_erc1155Address).balanceOf(address(this), _fractionId) >= 1, "Insufficient balance");
-        // ** should check balance
         IERC1155(_erc1155Address).safeTransferFrom(address(this), msg.sender, _fractionId, 1, "");
         pixelClaimed[_erc1155Address][_pixelId] = true;
     }
@@ -45,19 +44,15 @@ contract FractionManager is Initializable, PausableUpgradeable, OwnableUpgradeab
         return pixelClaimed[_erc1155Address][_pixelId];
     }
 
-    function pause() public onlyOwner {
-        _pause();
-    }
-
-    function unpause() public onlyOwner {
-        _unpause();
-    }
-
     function withdraw(address _erc1155Address, uint256 _fractionId) public onlyOwner {
         uint256 balance = IERC1155(_erc1155Address).balanceOf(address(this), _fractionId);
         IERC1155(_erc1155Address).safeTransferFrom(address(this), msg.sender, _fractionId, balance, "");
     }
-    
+
+    function setIsTokenClaimable(address _erc1155Address, bool canClaim) public onlyOwner {
+        isClaimOpen[_erc1155Address] = canClaim;
+    }
+
     function onERC1155Received(
         address,
         address,
@@ -68,13 +63,13 @@ contract FractionManager is Initializable, PausableUpgradeable, OwnableUpgradeab
         return this.onERC1155Received.selector;
     }
 
-    
+
     function onERC1155BatchReceived(
         address ,
         address ,
         uint256[] calldata ,
         uint256[] calldata ,
-        bytes calldata 
+        bytes calldata
     ) external virtual override returns (bytes4) {
         return this.onERC1155BatchReceived.selector;
     }
--- packages/hardhat/test/fractional.js
+++ packages/hardhat/test/fractional.js
@@ -4,62 +4,65 @@ const {ethers, upgrades} = require("hardhat");
 describe("Fractional Contract", function () {
   let mockPixelContract, signers;
   let fractionalManager;
-  let fractionalContract;
-
-  // const getTokenIdFromReceipt = (receipt) => {
-  //   return BigNumber.from(receipt.logs[0].topics[3]).toNumber()
-  // }
+  let mockERC1155Contract;
 
   before(async () => {
     signers = await ethers.getSigners()
 
-    // This is only for mock pixel contract.
-    console.log("\ndeploying mockPixelContract")
     const MockPixelContract = await ethers.getContractFactory("MockPixel");
     mockPixelContract = await upgrades.deployProxy(MockPixelContract, []);
     await mockPixelContract.deployed();
+    console.log('Mock Pixel deployed')
 
-    // console.log("mockPixelContract deployed to:", mockPixelContract.address);
-    console.log("\ndeploying mock Fractional contract")
-    const FractionalFactory = await ethers.getContractFactory("MockFraction");
-    fractionalContract = await FractionalFactory.deploy();
-    await fractionalContract.deployed();
-    console.log("mockFractionalContract deployed to:", fractionalContract.address);
+    const ERC1155Factory = await ethers.getContractFactory("MockFraction");
+    mockERC1155Contract = await ERC1155Factory.deploy();
+    await mockERC1155Contract.deployed();
+    console.log('Mock ERC1155 deployed')
 
-    await fractionalContract.mint(1, 100);
-    await fractionalContract.mint(2, 100);
+    await mockERC1155Contract.mint(1, 100);
+    await mockERC1155Contract.mint(2, 100);
 
-    console.log("\ndeploying fractionalManager")
     const FractionManagerFactory = await ethers.getContractFactory("FractionManager");
     fractionalManager = await upgrades.deployProxy(FractionManagerFactory, [mockPixelContract.address]);
     await fractionalManager.deployed();
-    console.log("FractionalManager deployed to:", fractionalManager.address);
+    console.log('Fractional Manager deployed')
   })
 
  it("Should deposit Fraction NFTs", async function () {
-    await fractionalContract.setApprovalForAll(fractionalManager.address, true);
-    await fractionalManager.deposit(fractionalContract.address, 1,  100);
-    expect(await fractionalContract.balanceOf(fractionalManager.address, 1)).to.equal(100);
+    await mockERC1155Contract.setApprovalForAll(fractionalManager.address, true);
+    await fractionalManager.deposit(mockERC1155Contract.address, 1,  100);
+    expect(await mockERC1155Contract.balanceOf(fractionalManager.address, 1)).to.equal(100);
   });
+
+  it("Should not be able to claim yet", async function() {
+    await expect(fractionalManager.claim(mockERC1155Contract.address, 1, 1)).to.be.revertedWith("Claim is not open");
+  })
 
   it("Should claim Fraction NFTs", async function () {
     await mockPixelContract.mint();
-    await fractionalManager.claim(fractionalContract.address, 1, 1);
-    expect(await fractionalContract.balanceOf(fractionalManager.address, 1)).to.equal(99);
-    expect(await fractionalContract.balanceOf(signers[0].address, 1)).to.equal(1);
+    await mockPixelContract.mint();
+    await fractionalManager.setIsTokenClaimable(mockERC1155Contract.address, true);
+    await fractionalManager.claim(mockERC1155Contract.address, 1, 1);
+    // fractional manager should have one less token
+    expect(await mockERC1155Contract.balanceOf(fractionalManager.address, 1)).to.equal(99);
+    // signer should have been sent a token
+    expect(await mockERC1155Contract.balanceOf(signers[0].address, 1)).to.equal(1);
   });
 
-  it("Should not claim more than one ERC1155 per a pixel", async function () {
-    await expect(fractionalManager.claim(fractionalContract.address, 1, 1)).to.be.revertedWith("Pixel already claimed");
+  it("Should not claim more than one ERC1155 per a pixel", async function() {
+    await expect(fractionalManager.claim(mockERC1155Contract.address, 1, 1)).to.be.revertedWith("Pixel already claimed");
   });
 
-  it("Should withdraw all ERC1155", async function () {
-    await expect(fractionalManager.connect(signers[1]).withdraw(fractionalContract.address, 1)).to.be.revertedWith("Ownable: caller is not the owner");
+  it("Should not be able to claim if caller is not holding a pixel", async function() {
+    const newContract = await fractionalManager.connect(signers[1])
+    await expect(newContract.claim(mockERC1155Contract.address, 1, 2)).to.be.revertedWith("Not pixel owner")
+  })
 
-    await fractionalManager.withdraw(fractionalContract.address, 1);
-    expect(await fractionalContract.balanceOf(fractionalManager.address, 1)).to.equal(0);
-    expect(await fractionalContract.balanceOf(signers[0].address, 1)).to.equal(100);
-  });
-   
- 
+  // it("Should withdraw all ERC1155", async function () {
+  //   await expect(fractionalManager.connect(signers[1]).withdraw(mockERC1155Contract.address, 1)).to.be.revertedWith("Ownable: caller is not the owner");
+  //
+  //   await fractionalManager.withdraw(mockERC1155Contract.address, 1);
+  //   expect(await mockERC1155Contract.balanceOf(fractionalManager.address, 1)).to.equal(0);
+  //   expect(await mockERC1155Contract.balanceOf(signers[0].address, 1)).to.equal(100);
+  // });
 });
