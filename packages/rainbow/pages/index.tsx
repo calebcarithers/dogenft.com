0654415c53fe5dcbc8c6a7efba786bfb0c73c056
Caleb Carithers <calebcarithers@me.com>
2022-10-16 02:27:48 -0500

    up

--- packages/rainbow/pages/index.tsx
+++ packages/rainbow/pages/index.tsx
@@ -227,11 +227,11 @@ const swapStyle = css("bg-doge-blue")
 const Pill: React.FC<PropsWithChildren<{ type: "donation" | "swap" }>> = ({type}) => {
   return <span
     style={{borderWidth: "1px"}}
-    className={css("inline-block", "font-normal", "text-sm", "border-black", "rounded-full", "px-1", {
+    className={css("inline-block", "font-normal", "text-sm", "border-black", "rounded-full", "px-2", "py-0.5", {
       [donationStyle]: type === "donation",
       [swapStyle]: type === "swap"
     })}>
-    {type === "donation" ? "donation" : "swap"}
+    {type === "donation" ? "donation" : "🌈 swap"}
   </span>
 }
 
