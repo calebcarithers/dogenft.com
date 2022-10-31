bc8ac161ee992460e0dfdfb725e5a8471d67705e
Caleb Carithers <calebcarithers@me.com>
2022-10-31 16:18:00 -0500

    up

--- packages/rainbow/pages/_app.tsx
+++ packages/rainbow/pages/_app.tsx
@@ -1,5 +1,6 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import "dsl/styles/globals.css";
 import type { AppProps } from 'next/app';
 import { ToastContainer } from 'react-toastify';
 import "react-toastify/dist/ReactToastify.css";
