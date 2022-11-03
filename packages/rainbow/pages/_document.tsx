34123267bf2de7f6e75d249b442a25e4f294768c
Caleb Carithers <calebcarithers@me.com>
2022-11-03 00:55:35 -0500

    meta

--- packages/rainbow/pages/_document.tsx
+++ packages/rainbow/pages/_document.tsx
@@ -1,6 +1,5 @@
 import { css } from "dsl/helpers/css";
 import { Head, Html, Main, NextScript } from 'next/document';
-import { isProd } from "../environment/vars";
 
 
 export const toastifyPortalId = "react-toastify-portal"
@@ -19,10 +18,10 @@ export default function Document() {
         <meta property="og:title" content="Bronze The Doge"/>
         <meta property="og:description" content="Help us raise funds to build Kabosu a statue in Japan."/>
         <meta property="og:image" content="/images/twitter-card.png"/>
-        <meta property="og:url" content={isProd() ? "https://bronzethedoge.xyz" : "https://dev.bronzethedoge.xyz"}/>
+        <meta property="og:url" content={"https://bronzethedoge.xyz"}/>
         <meta name="twitter:title" content="Bronze The Doge"/>
         <meta name="twitter:description" content="Help us raise funds to build Kabosu a statue in Japan."/>
-        <meta name="twitter:image" content="/images/twitter-card.png"/>
+        <meta name="twitter:image" content="https://bronzethedoge.xyz/images/twitter-card.png"/>
         <meta name="twitter:card" content="summary_large_image"/>
         <meta name="twitter:site" content="@bronzethedoge"/>
       </Head>
