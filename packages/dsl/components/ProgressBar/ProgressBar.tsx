99d9424d9173956f98e1cad8fe4a1e622a19e102
Caleb Carithers <calebcarithers@me.com>
2022-11-07 12:57:18 -0600

    stretch goals added

--- packages/dsl/components/ProgressBar/ProgressBar.tsx
+++ packages/dsl/components/ProgressBar/ProgressBar.tsx
@@ -1,7 +1,12 @@
 import { css } from "../../helpers/css";
 //@ts-ignore
-import { ReactNode, useMemo, useRef } from "react";
+import { ReactNode, useMemo } from "react";
 import styles from "./ProgressBar.module.css";
+
+export interface ProgressStep {
+  renderLabel: () => ReactNode;
+  value: number;
+}
 
 interface ProgressBarProps {
   max: number;
@@ -11,33 +16,93 @@ interface ProgressBarProps {
   renderMinLabel?: () => ReactNode;
   renderMaxLabel?: () => ReactNode;
   renderNowLabel?: () => ReactNode;
+  steps: ProgressStep[];
+  renderThumb: (now: number) => ReactNode;
 }
 
-export const ProgressBar: React.FC<ProgressBarProps> = ({min, max, now, thumb, renderMinLabel, renderMaxLabel, renderNowLabel}) => {
-  const conatinerRef = useRef<HTMLDivElement | null>(null)
-  const thumbRef = useRef<HTMLDivElement>(null)
+export const NewProgressBar: React.FC<
+  Pick<ProgressBarProps, "steps" | "now" | "renderThumb" | "renderNowLabel">
+> = ({ steps, now, renderThumb, renderNowLabel }) => {
+  const min = steps![0];
+  const max = steps![steps!.length - 1];
 
-  const percentage = useMemo(() => {
-    return (now / (max - min)) * 100
-  }, [now, min, max])
+  const percentageComplete = useMemo(() => {
+    return (now / (max.value - min.value)) * 100;
+  }, [now, min, max]);
 
-  return <div className={css("relative", "my-14")}>
-    <div ref={conatinerRef}
-      className={css("h-[15px]", "bg-pixels-yellow-100", "w-full", "rounded-full", "border-[1px]", "border-black", "relative")}>
-      <div className={css("h-full", "bg-meme-yellow", "rounded-full", styles.rainbow)} style={{width: `${percentage}%`}}/>
+  return (
+    <div className={css("relative", "my-14")}>
       <div
-        ref={thumbRef}
-        style={{
-          left: `${percentage}%`
-        }}
-        className={css("absolute", "z-20", "w-[60px]", "h-[60px]", "top-1/2", "-translate-y-[50%]", "-translate-x-[50%]", "flex", "justify-center", "items-center")}>
-        {thumb ? thumb : <div className={css("text-3xl")}>🐕</div>}
+        className={css(
+          "h-[15px]",
+          "bg-pixels-yellow-100",
+          "w-full",
+          "rounded-full",
+          "border-[1px]",
+          "border-black",
+          "relative"
+        )}
+      >
+        <div
+          className={css(
+            "h-full",
+            "bg-meme-yellow",
+            "rounded-full",
+            styles.rainbow
+          )}
+          style={{ width: `${percentageComplete}%` }}
+        />
+        <div
+          style={{
+            left: `${percentageComplete}%`,
+          }}
+          className={css(
+            "absolute",
+            "z-20",
+            "w-[50px]",
+            "h-[50px]",
+            "top-1/2",
+            "-translate-y-[50%]",
+            "-translate-x-[50%]",
+            "flex",
+            "justify-center",
+            "items-center"
+          )}
+        >
+          {renderThumb(now)}
+        </div>
       </div>
+      <div
+        className={css("absolute", "top-28", "-translate-x-[50%]", "font-bold")}
+        style={{
+          left: `${percentageComplete}%`,
+        }}
+      >
+        {renderNowLabel ? renderNowLabel() : now}
+      </div>
+      {steps.map((step, index, arr) => {
+        const stepPercentage = (step.value / (max.value - min.value)) * 100;
+        return (
+          <div
+            style={{
+              left: `${stepPercentage}%`,
+            }}
+            key={`step-label-${step.value}`}
+            className={css(
+              "z-10",
+              "absolute",
+              "top-0",
+              "-translate-y-[50%]",
+              "-translate-x-[50%]",
+              "w-[100px]"
+            )}
+          >
+            {step.renderLabel()}
+          </div>
+        );
+      })}
+      {/* <div className={css("absolute", "left-0", "top-14")}>{renderMinLabel ? renderMinLabel() : min}</div> */}
+      {/* <div className={css("absolute", "right-0", "top-14")}>{renderMaxLabel ? renderMaxLabel() : max}</div> */}
     </div>
-    <div className={css("absolute", "top-24", "-translate-x-[50%]", "font-bold",)} style={{
-      left: `${percentage}%`
-    }}>{renderNowLabel ? renderNowLabel() : now}</div>
-    <div className={css("absolute", "left-0", "top-14")}>{renderMinLabel ? renderMinLabel() : min}</div>
-    <div className={css("absolute", "right-0", "top-14")}>{renderMaxLabel ? renderMaxLabel() : max}</div>
-  </div>
-}
+  );
+};
