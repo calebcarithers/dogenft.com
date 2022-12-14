e3c6f37e75ffae071f4dfc73de658117c91dbbce
Caleb Carithers <calebcarithers@me.com>
2022-12-14 16:09:49 -0600

    wrap nounlet fn

--- packages/hardhat/test/nounlet-wrapper.js
+++ packages/hardhat/test/nounlet-wrapper.js
@@ -36,32 +36,33 @@ describe("Nounlet Wrapper", function() {
         return contract.connect(signer)
     }
 
-    it("Wraps nounlet", async function() {
-        const {contract: wrapperContractOwner, owner} = await loadFixture(deployContractFixture)
-        const testNounlet = {id: 69, ownerAddress: "0xaF46dc96bd783E683fD0EFeF825e6110165b8f9E"}
-        const nounletContract = await getNounletOwner(testNounlet.id)
-        expect(nounletContract.signer.address).eq(testNounlet.ownerAddress)
-
-        const tokenURI = await nounletContract.uri(testNounlet.id)
-        const contractURI = await nounletContract.contractURI();
-
+    async function wrapNounlet({nounletContract, wrapperContract, id}) {
         // give approval to wrapper contract to move our nounlet
-        const tx = await nounletContract.setApprovalForAll(wrapperContractOwner.address, true);  
-        await tx.wait()      
+        await nounletContract.setApprovalForAll(wrapperContract.address, true);
 
         // wrap nounlet
-        await wrapperContractOwner.connect(nounletContract.signer).wrap(testNounlet.id, {gasLimit: 2100000});
+        await wrapperContract.connect(nounletContract.signer).wrap(id, {gasLimit: 2100000});
 
-        // check tokenURI and contractURI
-        const wrappedTokenURI = await wrapperContractOwner.tokenURI(testNounlet.id);
-        const wrappedContractURI = await wrapperContractOwner.contractURI();
-
-        expect(tokenURI).to.eq(wrappedTokenURI)
+        // assert uri/tokenURI & contractURI
+        const uri = await nounletContract.uri(id)
+        const contractURI = await nounletContract.contractURI();
+        const wrappedTokenURI = await wrapperContract.tokenURI(id);
+        const wrappedContractURI = await wrapperContract.contractURI();
+        expect(uri).to.eq(wrappedTokenURI)
         expect(contractURI).to.eq(wrappedContractURI)
 
         // previous nounlet owner should be owner of wrapped nounlet
-        expect(await wrapperContractOwner.ownerOf(testNounlet.id)).to.eq(nounletContract.signer.address)
+        expect(await wrapperContract.ownerOf(id)).to.eq(nounletContract.signer.address)
         // wrapper contract should be owner of the nounlet
-        expect(await nounletContract.balanceOf(wrapperContractOwner.address, testNounlet.id)).to.eq(1)
+        expect(await nounletContract.balanceOf(wrapperContract.address, id)).to.eq(1)
+    }
+
+    it("Wraps nounlet", async function() {
+        const testNounlet = {id: 69, ownerAddress: "0xaF46dc96bd783E683fD0EFeF825e6110165b8f9E"}
+        const {contract: wrapperContract} = await loadFixture(deployContractFixture)
+        const nounletContract = await getNounletOwner(testNounlet.id)
+        expect(nounletContract.signer.address).eq(testNounlet.ownerAddress)
+
+        await wrapNounlet({nounletContract, wrapperContract, id: testNounlet.id})
     })
 })
