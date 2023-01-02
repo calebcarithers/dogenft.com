d4f75d79ae45723c91f8d46e1df7ad9ddd86b949
Caleb Carithers <calebcarithers@me.com>
2023-01-01 20:33:35 -0500

    nounlet up

--- devtools/scratchpad/snapShotPixelHolders.js
+++ devtools/scratchpad/snapShotPixelHolders.js
@@ -1,52 +1,52 @@
-const axios = require("axios")
-const keccak256 = require("keccak256")
-const { default: MerkleTree } = require("merkletreejs")
-const fs = require("fs")
-
+const axios = require("axios");
+const keccak256 = require("keccak256");
+const { default: MerkleTree } = require("merkletreejs");
+const fs = require("fs");
 
 const getPixelHolderAddresses = async () => {
-    const { data } = await axios.get("https://api.ownthedoge.com/v1/config")
-    const addresses = Object.keys(data)
-    return addresses.filter(address => data[address].tokenIds.length > 0)
-}
+  const { data } = await axios.get("https://api.ownthedoge.com/v1/config");
+  const addresses = Object.keys(data);
+  return addresses.filter((address) => data[address].tokenIds.length > 0);
+};
 
 const generateMerkleRoot = (addresses) => {
-    const leafNodes = addresses.map(address => keccak256(address))
-    const merkleTree = new MerkleTree(leafNodes, keccak256, { sortPairs: true })
-    return merkleTree.getHexRoot()
-}
+  const leafNodes = addresses.map((address) => keccak256(address));
+  const merkleTree = new MerkleTree(leafNodes, keccak256, { sortPairs: true });
+  return merkleTree.getHexRoot();
+};
 
 const writeJsonBlobToFile = (name, data) => {
   return fs.writeFile(name, JSON.stringify(data), (err) => {
-      if (err) {
-          console.error(err)
-      }
-  })
-}
+    if (err) {
+      console.error(err);
+    }
+  });
+};
 
 const main = async () => {
-    // const addresses = await getPixelHolderAddresses()
-    const addresses = [
-        "0x12E9d84aF808C26F21e383af5762F48b990aDC09",
-        "0xd801d86C10e2185a8FCBccFB7D7baF0A6C5B6BD5",
-        "0xAd3c410Df6F60d61DEDf7202e8e4805C79EBf54a",
-        "0xC096df02D3C765f6C720EBef5b947c72Ab0E6B65",
-        "0x77509a9adFB1A85C2113eE80FBb195d771dcdFc2",
-        "0xC096df02D3C765f6C720EBef5b947c72Ab0E6B65",
-        "0x901e7cbA2605CD3C125dFeD78d139A26bEf23325",
-        "0x0baF1F0F6ba5d83615f51a7Dc28bf12C98A196F1",
-        "0x26562efa88602a717A3A42523d6ff8CFf2806420"
-    ]
+  // const addresses = await getPixelHolderAddresses()
+  const addresses = [
+    "0x12E9d84aF808C26F21e383af5762F48b990aDC09",
+    "0xd801d86C10e2185a8FCBccFB7D7baF0A6C5B6BD5",
+    "0xAd3c410Df6F60d61DEDf7202e8e4805C79EBf54a",
+    "0xC096df02D3C765f6C720EBef5b947c72Ab0E6B65",
+    "0x77509a9adFB1A85C2113eE80FBb195d771dcdFc2",
+    "0xC096df02D3C765f6C720EBef5b947c72Ab0E6B65",
+    "0x901e7cbA2605CD3C125dFeD78d139A26bEf23325",
+    "0x0baF1F0F6ba5d83615f51a7Dc28bf12C98A196F1",
+    "0x26562efa88602a717A3A42523d6ff8CFf2806420",
+  ];
 
-    const merkleRoot = generateMerkleRoot(addresses)
-    console.log("address of length", addresses.length, "found")
-    console.log("got merkle root", merkleRoot)
-    
-    const filename = "lord-of-dogetown-whitelist-hardhat.json"
-    writeJsonBlobToFile(`../../packages/ownthedoge/services/whitelists/${filename}`, addresses)
-    writeJsonBlobToFile(`./${filename}`, addresses)
-}
+  const merkleRoot = generateMerkleRoot(addresses);
+  console.log("address of length", addresses.length, "found");
+  console.log("got merkle root", merkleRoot);
 
+  const filename = "lord-of-dogetown-whitelist-hardhat.json";
+  writeJsonBlobToFile(
+    `../../packages/ownthedoge/services/whitelists/${filename}`,
+    addresses
+  );
+  writeJsonBlobToFile(`./${filename}`, addresses);
+};
 
-main()
-    
+main();
