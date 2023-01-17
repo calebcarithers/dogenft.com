c0c0ac432e8e62593e6780ea5936f98761d98696
Caleb Carithers <calebcarithers@me.com>
2023-01-17 12:37:41 -0600

    up

--- packages/rainbow/pages/_app.tsx
+++ packages/rainbow/pages/_app.tsx
@@ -1,40 +1,42 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-import type { AppProps } from 'next/app';
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import type { AppProps } from "next/app";
 import Script from "next/script";
-import { ToastContainer } from 'react-toastify';
+import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { vars } from "../environment/vars";
-import '../styles/globals.css';
+import "../styles/globals.css";
 
+const queryClient = new QueryClient();
 
-const queryClient = new QueryClient()
-
-function MyApp({Component, pageProps}: AppProps) {
-  return <>
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <>
       <Script
-            id={"gatag"}
-            strategy="lazyOnload"
-            src={`https://www.googletagmanager.com/gtag/js?id=${vars.gaId}`}/>
+        id={"gatag"}
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=${vars.gaId}`}
+      />
 
       <Script id={"gatagsomethingelse"} strategy="lazyOnload">
-          {`
+        {`
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
           gtag('config', '${vars.gaId}');
               `}
-        </Script>
-    <QueryClientProvider client={queryClient}>
-      <ReactQueryDevtools initialIsOpen={false}/>
-      <Component {...pageProps} />
-    </QueryClientProvider>
-    <ToastContainer 
-    position={"bottom-center"} 
-    autoClose={1500} 
-    closeButton={<></>}
-    />
-  </>
+      </Script>
+      <QueryClientProvider client={queryClient}>
+        <ReactQueryDevtools initialIsOpen={false} />
+        <Component {...pageProps} />
+      </QueryClientProvider>
+      <ToastContainer
+        position={"bottom-center"}
+        autoClose={1500}
+        closeButton={<></>}
+      />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
--- packages/rainbow/pages/_document.tsx
+++ packages/rainbow/pages/_document.tsx
@@ -1,8 +1,7 @@
-import { css } from 'dsl/helpers/css';
-import { Head, Html, Main, NextScript } from 'next/document';
+import { css } from "dsl/helpers/css";
+import { Head, Html, Main, NextScript } from "next/document";
 
-
-export const toastifyPortalId = "react-toastify-portal"
+export const toastifyPortalId = "react-toastify-portal";
 
 export default function Document() {
   return (
@@ -13,21 +12,33 @@ export default function Document() {
           content="Help us raise funds to build Kabosu a statue in Japan."
           key="desc"
         />
-        <meta property="og:site_name" content="Bronze The Doge"/>
-        <meta property="og:title" content="Bronze The Doge"/>
-        <meta property="og:description" content="Help us raise funds to build Kabosu a statue in Japan."/>
-        <meta property="og:image" content="https://bronzethedoge.xyz/images/twitter-card.png"/>
-        <meta property="og:url" content="https://bronzethedoge.xyz"/>
-        <meta name="twitter:title" content="Bronze The Doge"/>
-        <meta name="twitter:description" content="Help us raise funds to build Kabosu a statue in Japan."/>
-        <meta name="twitter:image" content="https://bronzethedoge.xyz/images/twitter-card.png"/>
-        <meta name="twitter:card" content="summary_large_image"/>
-        <meta name="twitter:site" content="@bronzethedoge"/>
+        <meta property="og:site_name" content="Bronze The Doge" />
+        <meta property="og:title" content="Bronze The Doge" />
+        <meta
+          property="og:description"
+          content="Help us raise funds to build Kabosu a statue in Japan."
+        />
+        <meta
+          property="og:image"
+          content="https://bronzethedoge.xyz/images/twitter-card.png"
+        />
+        <meta property="og:url" content="https://bronzethedoge.xyz" />
+        <meta name="twitter:title" content="Bronze The Doge" />
+        <meta
+          name="twitter:description"
+          content="Help us raise funds to build Kabosu a statue in Japan."
+        />
+        <meta
+          name="twitter:image"
+          content="https://bronzethedoge.xyz/images/twitter-card.png"
+        />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="@bronzethedoge" />
       </Head>
-      <body className={css("font-ComicNeue")}>
-        <Main/>
-        <NextScript/>
+      <body className={css("font-ComicNeue", "!mr-0")}>
+        <Main />
+        <NextScript />
       </body>
     </Html>
-  )
+  );
 }
