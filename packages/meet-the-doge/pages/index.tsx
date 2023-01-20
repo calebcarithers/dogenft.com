76dc27bed9e34f4254ee3a76e0c5d0e52b25ff94
Caleb Carithers <calebcarithers@me.com>
2023-01-20 16:00:24 -0600

    up

--- packages/meet-the-doge/pages/index.tsx
+++ packages/meet-the-doge/pages/index.tsx
@@ -1,10 +1,7 @@
-import { Inter } from "@next/font/google";
 import Button from "dsl/components/Button/Button";
 import Link from "dsl/components/Link/Link";
 import { css } from "dsl/helpers/css";
 import Head from "next/head";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
