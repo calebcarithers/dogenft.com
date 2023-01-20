458a64475e71ef59377a45c7a9a154979aa208b8
Caleb Carithers <calebcarithers@me.com>
2023-01-20 17:43:20 -0600

    social card

--- packages/meet-the-doge/pages/_document.tsx
+++ packages/meet-the-doge/pages/_document.tsx
@@ -1,13 +1,41 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Head, Html, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta
+          name="description"
+          content="Your chance to meet Kabosu in her hometown."
+          key="desc"
+        />
+        <meta property="og:site_name" content="Meet The Doge" />
+        <meta property="og:title" content="Meet The Doge" />
+        <meta
+          property="og:description"
+          content="Your chance to meet Kabosu in her hometown."
+        />
+        <meta
+          property="og:image"
+          content="https://meethedoge.com/images/twitter-card.png"
+        />
+        <meta property="og:url" content="https://meethedoge.com" />
+        <meta name="twitter:title" content="Meet The Doge" />
+        <meta
+          name="twitter:description"
+          content="Your chance to meet Kabosu in her hometown."
+        />
+        <meta
+          name="twitter:image"
+          content="https://meethedoge.com/images/twitter-card.png"
+        />
+        <meta name="twitter:card" content="summary_large_image" />
+        {/* <meta name="twitter:site" content="@bronzethedoge" /> */}
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
