ce6a57e2e2a3159af9d58daea497d991d8112de9
Caleb Carithers <calebcarithers@me.com>
2023-01-21 13:34:09 -0600

    up

--- packages/much-wow-film/next.config.js
+++ packages/much-wow-film/next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+};
 
-module.exports = nextConfig
+const withTm = require("next-transpile-modules")(["dsl", "ownthedoge"]);
+
+module.exports = withTm(nextConfig);
