bb3213d0e44e0ec76d0b864054a3d45331d67995
Caleb Carithers <calebcarithers@me.com>
2023-01-31 23:18:34 -0600

    up

--- packages/much-wow-film/pages/index.tsx
+++ packages/much-wow-film/pages/index.tsx
@@ -1,8 +1,17 @@
-import { PivotControls, useGLTF, useVideoTexture } from "@react-three/drei";
-import { Canvas } from "@react-three/fiber";
+import { Comic_Neue } from "@next/font/google";
+import { PivotControls, useVideoTexture } from "@react-three/drei";
+import { Canvas, useFrame } from "@react-three/fiber";
+import ColoredText from "dsl/components/ColoredText/ColoredText";
 import { css } from "dsl/helpers/css";
 import Head from "next/head";
-import { Suspense } from "react";
+import { Suspense, useRef } from "react";
+import { DoubleSide } from "three";
+
+const comicNeue = Comic_Neue({
+  weight: ["700"],
+  subsets: ["latin"],
+  variable: "--font-comic-neue",
+});
 
 export default function Home() {
   return (
@@ -13,56 +22,55 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main
-        className={css(
-          "bg-pixels-yellow-100",
-          "grow",
-          "flex",
-          "flex-col",
-          "items-center",
-          "justify-center"
-        )}
-      >
-        <div
-          className={css(
-            "relative",
-            "max-w-xl",
-            "w-full",
-            "shrink-0",
-            "border-[1px]",
-            "border-solid",
-            "border-black",
-            "h-[500px]"
-          )}
-        >
+      <main className={css("bg-[#4893ca]", "h-screen", "w-screen")}>
+        <div className={css("relative", "h-screen", "w-screen")}>
           <Canvas camera={{ position: [0, 0, 20] }} className={css("grow")}>
             <Suspense fallback={null}>
               <PivotControls visible={false} lineWidth={1} depthTest={false}>
-                <Model />
+                <Video />
               </PivotControls>
-              {/* <GizmoHelper alignment="bottom-right" margin={[80, 80]}>
-                <GizmoViewport
-                  axisColors={["red", "green", "blue"]}
-                  labelColor="black"
-                />
-              </GizmoHelper> */}
               <ambientLight intensity={0.5} />
             </Suspense>
           </Canvas>
+          <div
+            className={css(
+              "absolute",
+              "inset-0",
+              "w-full",
+              "h-full",
+              "flex",
+              "justify-center",
+              "items-center"
+            )}
+          >
+            <ColoredText
+              className={css(comicNeue.className, "text-3xl", "md:text-5xl")}
+            >
+              something is coming
+            </ColoredText>
+          </div>
         </div>
       </main>
     </>
   );
 }
 
-const Model = () => {
-  const { nodes, materials, scene } = useGLTF("./models/doge.glb");
+const Video = () => {
   const texture = useVideoTexture("./videos/wow.mp4", { autoplay: true });
+  const ref = useRef<THREE.Mesh | null>(null);
+  useFrame(() => {
+    if (ref.current !== null) {
+      ref.current.rotation.y += 0.02;
+    }
+  });
+
+  const aspectRatio = 1.777777778;
+  const height = 10;
+  const width = aspectRatio * height;
   return (
-    <mesh position={[0, 0, 18.5]}>
-      {/* <primitive object={scene} /> */}
-      <planeGeometry args={[1.777777778, 1]} />
-      <meshBasicMaterial map={texture} toneMapped={false} />
+    <mesh ref={ref} position={[0, 0, 0]}>
+      <planeGeometry args={[width, height]} />
+      <meshBasicMaterial map={texture} toneMapped={false} side={DoubleSide} />
     </mesh>
   );
 };
