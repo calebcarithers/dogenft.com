364f539d7c20d5449768a40e73b59424f8b16855
Caleb Carithers <calebcarithers@me.com>
2023-02-01 00:24:08 -0600

    fix

--- packages/dsl/components/ColoredText/ColoredText.tsx
+++ packages/dsl/components/ColoredText/ColoredText.tsx
@@ -1,11 +1,11 @@
-import React, {PropsWithChildren} from "react";
-import {objectKeys} from "../../helpers/arrays";
-import {css} from "../../helpers/css";
+import React, { PropsWithChildren } from "react";
+import { objectKeys } from "../../helpers/arrays";
+import { css } from "../../helpers/css";
 
-const tailwindconfig = require("../../tailwind.config.cjs")
+const tailwindconfig = require("../../tailwind.config.cjs");
 
-const dogeColors = tailwindconfig.theme.extend.colors.doge
-const dogeColorNames = objectKeys(dogeColors)
+const dogeColors = tailwindconfig.theme.extend.colors.doge;
+const dogeColorNames = objectKeys(dogeColors);
 
 interface ColoredTextProps {
   children: string;
@@ -13,21 +13,32 @@ interface ColoredTextProps {
   className?: string;
 }
 
-const ColoredText: React.FC<PropsWithChildren<ColoredTextProps>> = ({children, className, bold}) => {
-  return <span className={css(className)}>
-    {[...children].map((char, index) => {
-      const colorName = dogeColorNames[Math.floor(Math.random() * dogeColorNames.length)]
-      const color = dogeColors[colorName]
-      return <span
-        className={css({
-          "font-bold": bold
+const ColoredText: React.FC<PropsWithChildren<ColoredTextProps>> = ({
+  children,
+  className,
+  bold,
+}) => {
+  return (
+    <span className={css(className)}>
+      {Array.isArray(children) &&
+        [...children].map((char, index) => {
+          const colorName =
+            dogeColorNames[Math.floor(Math.random() * dogeColorNames.length)];
+          const color = dogeColors[colorName];
+          return (
+            <span
+              className={css({
+                "font-bold": bold,
+              })}
+              key={`nav-item-selection-${char}-${index}`}
+              style={{ color: color }}
+            >
+              {char}
+            </span>
+          );
         })}
-        key={`nav-item-selection-${char}-${index}`}
-        style={{color: color}}>
-          {char}
-        </span>
-    })}
-  </span>
-}
+    </span>
+  );
+};
 
-export default ColoredText
+export default ColoredText;
