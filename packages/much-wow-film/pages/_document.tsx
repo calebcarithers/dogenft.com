72fcfda8e417c9303cdc056d9ecd08d8c5d8b634
Caleb Carithers <calebcarithers@me.com>
2023-02-01 00:15:44 -0600

    up

--- packages/much-wow-film/pages/_document.tsx
+++ packages/much-wow-film/pages/_document.tsx
@@ -1,13 +1,30 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Head, Html, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta name="description" content="Something is coming." key="desc" />
+        <meta property="og:site_name" content="Much Wow Film" />
+        <meta property="og:title" content="Much Wow Film" />
+        <meta property="og:description" content="Something is coming." />
+        <meta
+          property="og:image"
+          content="https://muchwowfilm.com/images/twitter-card.png"
+        />
+        <meta property="og:url" content="https://muchwowfilm.com" />
+        <meta name="twitter:title" content="Meet The Doge" />
+        <meta name="twitter:description" content="Something is coming." />
+        <meta
+          name="twitter:image"
+          content="https://muchwowfilm.com/images/twitter-card.png"
+        />
+        <meta name="twitter:card" content="summary_large_image" />
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
