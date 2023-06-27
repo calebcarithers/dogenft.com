e5e81e43863b6750e051b7cc314915453263aae1
Caleb Carithers <calebcarithers@me.com>
2023-06-26 23:09:42 -0500

    up

---
+++ packages/doge-gpt/demoData.ts
@@ -0,0 +1,99 @@
+const demoData = [
+  {
+    prompt: "asdfasd",
+    date: new Date(),
+    isLoading: false,
+    id: 0.13464677869326214,
+    response:
+      "\n\nWow, much doge, such style! Very amaze, asdfasd is so cool! Much fun, such amazing! Wow!",
+  },
+  {
+    prompt: "aasdfasdf",
+    date: new Date(),
+    isLoading: false,
+    id: 0.35395498326862485,
+    response:
+      "\n\nWow, such amaze! Very doge. Much style. Much in. Such asdfasdf. Very aasdfasdf. Wow!",
+  },
+  {
+    prompt: "asdf",
+    date: new Date(),
+    isLoading: false,
+    id: 0.011668142907067303,
+    response:
+      "\n\nWow, such asdf, very computer science, much programming, amaze algorithms.",
+  },
+  {
+    prompt: "asdfasdfasdfasdf",
+    date: new Date(),
+    isLoading: false,
+    id: 0.6037445976094804,
+    response:
+      "\n\nwow, such amaze, very doge. much style, so wow. very asdfasdfasdfasdf, much awesome. so much awesome, wow.",
+  },
+  {
+    prompt: "asdfasdfasdf",
+    date: new Date(),
+    isLoading: false,
+    id: 0.4877229747587162,
+    response:
+      "\n\nWow, such doge amaze! Much style, very words. So many wow, much amaze. Such doge, very cool. Much awesome, very fun. Wow!",
+  },
+  {
+    prompt: "wow tell me something super sexy in 100words or less please",
+    date: new Date(),
+    isLoading: false,
+    id: 0.6825256531414252,
+    response:
+      "\n\nWow, such sexy! Much desire! Very alluring! Such temptation! Such sensuality! Much enthralling! Very captivating! Amaze with beauty! Much pleasure! Such seduction! Very inviting! Much passion! Very inviting! Much arousal! Very stimulating! Much pleasure! Very exciting! Much anticipation! Very enthralling! Much pleasure! Very tantalizing!",
+  },
+  {
+    prompt: "asdfasd",
+    date: new Date(),
+    isLoading: false,
+    id: 0.13464677869326214,
+    response:
+      "\n\nWow, much doge, such style! Very amaze, asdfasd is so cool! Much fun, such amazing! Wow!",
+  },
+  {
+    prompt: "aasdfasdf",
+    date: new Date(),
+    isLoading: false,
+    id: 0.35395498326862485,
+    response:
+      "\n\nWow, such amaze! Very doge. Much style. Much in. Such asdfasdf. Very aasdfasdf. Wow!",
+  },
+  {
+    prompt: "asdf",
+    date: new Date(),
+    isLoading: false,
+    id: 0.011668142907067303,
+    response:
+      "\n\nWow, such asdf, very computer science, much programming, amaze algorithms.",
+  },
+  {
+    prompt: "asdfasdfasdfasdf",
+    date: new Date(),
+    isLoading: false,
+    id: 0.6037445976094804,
+    response:
+      "\n\nwow, such amaze, very doge. much style, so wow. very asdfasdfasdfasdf, much awesome. so much awesome, wow.",
+  },
+  {
+    prompt: "asdfasdfasdf",
+    date: new Date(),
+    isLoading: false,
+    id: 0.4877229747587162,
+    response:
+      "\n\nWow, such doge amaze! Much style, very words. So many wow, much amaze. Such doge, very cool. Much awesome, very fun. Wow!",
+  },
+  {
+    prompt: "wow tell me something super sexy in 100words or less please",
+    date: new Date(),
+    isLoading: false,
+    id: 0.6825256531414252,
+    response:
+      "\n\nWow, such sexy! Much desire! Very alluring! Such temptation! Such sensuality! Much enthralling! Very captivating! Amaze with beauty! Much pleasure! Such seduction! Very inviting! Much passion! Very inviting! Much arousal! Very stimulating! Much pleasure! Very exciting! Much anticipation! Very enthralling! Much pleasure! Very tantalizing!",
+  },
+];
+export default demoData;
--- packages/doge-gpt/pages/index.tsx
+++ packages/doge-gpt/pages/index.tsx
@@ -1,3 +1,4 @@
+import demoData from "@/demoData";
 import { Comic_Neue } from "@next/font/google";
 import ColoredText from "dsl/components/ColoredText/ColoredText";
 import { css } from "dsl/helpers/css";
@@ -234,7 +235,7 @@ interface Store {
 }
 
 const useStore = create<Store>((set) => ({
-  prompts: [],
+  prompts: demoData,
   post: async (prompt: string) => {
     try {
       const id = Math.random();
@@ -272,102 +273,3 @@ const useStore = create<Store>((set) => ({
     }
   },
 }));
-
-const demoData = [
-  {
-    prompt: "asdfasd",
-    date: new Date(),
-    isLoading: false,
-    id: 0.13464677869326214,
-    response:
-      "\n\nWow, much doge, such style! Very amaze, asdfasd is so cool! Much fun, such amazing! Wow!",
-  },
-  {
-    prompt: "aasdfasdf",
-    date: new Date(),
-    isLoading: false,
-    id: 0.35395498326862485,
-    response:
-      "\n\nWow, such amaze! Very doge. Much style. Much in. Such asdfasdf. Very aasdfasdf. Wow!",
-  },
-  {
-    prompt: "asdf",
-    date: new Date(),
-    isLoading: false,
-    id: 0.011668142907067303,
-    response:
-      "\n\nWow, such asdf, very computer science, much programming, amaze algorithms.",
-  },
-  {
-    prompt: "asdfasdfasdfasdf",
-    date: new Date(),
-    isLoading: false,
-    id: 0.6037445976094804,
-    response:
-      "\n\nwow, such amaze, very doge. much style, so wow. very asdfasdfasdfasdf, much awesome. so much awesome, wow.",
-  },
-  {
-    prompt: "asdfasdfasdf",
-    date: new Date(),
-    isLoading: false,
-    id: 0.4877229747587162,
-    response:
-      "\n\nWow, such doge amaze! Much style, very words. So many wow, much amaze. Such doge, very cool. Much awesome, very fun. Wow!",
-  },
-  {
-    prompt: "wow tell me something super sexy in 100words or less please",
-    date: new Date(),
-    isLoading: false,
-    id: 0.6825256531414252,
-    response:
-      "\n\nWow, such sexy! Much desire! Very alluring! Such temptation! Such sensuality! Much enthralling! Very captivating! Amaze with beauty! Much pleasure! Such seduction! Very inviting! Much passion! Very inviting! Much arousal! Very stimulating! Much pleasure! Very exciting! Much anticipation! Very enthralling! Much pleasure! Very tantalizing!",
-  },
-  {
-    prompt: "asdfasd",
-    date: new Date(),
-    isLoading: false,
-    id: 0.13464677869326214,
-    response:
-      "\n\nWow, much doge, such style! Very amaze, asdfasd is so cool! Much fun, such amazing! Wow!",
-  },
-  {
-    prompt: "aasdfasdf",
-    date: new Date(),
-    isLoading: false,
-    id: 0.35395498326862485,
-    response:
-      "\n\nWow, such amaze! Very doge. Much style. Much in. Such asdfasdf. Very aasdfasdf. Wow!",
-  },
-  {
-    prompt: "asdf",
-    date: new Date(),
-    isLoading: false,
-    id: 0.011668142907067303,
-    response:
-      "\n\nWow, such asdf, very computer science, much programming, amaze algorithms.",
-  },
-  {
-    prompt: "asdfasdfasdfasdf",
-    date: new Date(),
-    isLoading: false,
-    id: 0.6037445976094804,
-    response:
-      "\n\nwow, such amaze, very doge. much style, so wow. very asdfasdfasdfasdf, much awesome. so much awesome, wow.",
-  },
-  {
-    prompt: "asdfasdfasdf",
-    date: new Date(),
-    isLoading: false,
-    id: 0.4877229747587162,
-    response:
-      "\n\nWow, such doge amaze! Much style, very words. So many wow, much amaze. Such doge, very cool. Much awesome, very fun. Wow!",
-  },
-  {
-    prompt: "wow tell me something super sexy in 100words or less please",
-    date: new Date(),
-    isLoading: false,
-    id: 0.6825256531414252,
-    response:
-      "\n\nWow, such sexy! Much desire! Very alluring! Such temptation! Such sensuality! Much enthralling! Very captivating! Amaze with beauty! Much pleasure! Such seduction! Very inviting! Much passion! Very inviting! Much arousal! Very stimulating! Much pleasure! Very exciting! Much anticipation! Very enthralling! Much pleasure! Very tantalizing!",
-  },
-];
